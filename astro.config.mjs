11afcc182b9d8e6e67c573fb821a1aa9a94482de
Christian Adams <cadams@Christians-MacBook-Pro.local>
2023-06-11 16:01:11 -0500

    moving on to deleting old posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,31 @@ import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import image from '@astrojs/image'
-
 import tailwind from '@astrojs/tailwind'
+import addClasses from 'rehype-add-classes'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.cadams.dev/',
-  integrations: [sitemap(), react(), image(), tailwind()]
+  site: 'https://www.cmadams.dev/',
+  integrations: [sitemap(), react(), image(), tailwind()],
+  markdown: {
+    extendDefaultPlugins: true,
+    rehypePlugins: [
+      [
+        addClasses,
+        {
+          h1: 'text-4xl font-bold font-spaceMono',
+          h2: 'text-2xl font-bold font-spaceMono',
+          h3: 'text-xl font-bold font-spaceMono',
+          h4: 'text-lg font-bold font-spaceMono',
+          h5: 'font-bold font-spaceMono',
+          h6: 'font-bold font-spaceMono',
+          img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
+          p: 'mb-6',
+          a: 'underline underline-offset-2 hover:text-amber-500 decoration-amber-500',
+          ul: 'list-disc'
+        }
+      ]
+    ]
+  }
 })
